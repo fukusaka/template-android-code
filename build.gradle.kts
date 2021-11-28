cdf9d4e95ff614ac3549d940742eb462aa3e3fe0
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2021-11-28 14:05:49 +0900

    update dagger 2.38.1 for fixed incompatibility gradle 7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 buildscript {
     val kotlin_version by extra("1.5.21")
     val ktlint_version by extra("0.41.0")
-    val hilt_version by extra("2.38")
+    val hilt_version by extra("2.38.1")
 
     repositories {
         google()
