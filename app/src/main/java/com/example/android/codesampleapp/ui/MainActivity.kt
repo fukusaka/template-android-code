53a809214a4220a7909f73d50af08e6504b69c24
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2022-03-04 00:21:56 +0900

    MainActivity のパッケージ位置変更

--- app/src/main/java/com/example/android/codesampleapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/codesampleapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android.codesampleapp
+package com.example.android.codesampleapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.codesampleapp.R
 import com.example.android.codesampleapp.ui.main.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
 
