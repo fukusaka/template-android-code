8a68d118d40a2d2b5a70c59e6c3fbe3ec5ebeead
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2022-03-04 02:13:52 +0900

    dataBinding のサンプル

--- app/src/main/java/com/example/android/codesampleapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/android/codesampleapp/ui/main/MainFragment.kt
@@ -1,14 +1,25 @@
 package com.example.android.codesampleapp.ui.main
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.codesampleapp.R
+import com.example.android.codesampleapp.databinding.MainFragmentBinding
+import com.example.android.codesampleapp.ext.dataBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainFragment : Fragment(R.layout.main_fragment) {
 
+    private val binding: MainFragmentBinding? by dataBinding()
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.viewModel = viewModel
+    }
 
     companion object {
         fun newInstance() = MainFragment()
