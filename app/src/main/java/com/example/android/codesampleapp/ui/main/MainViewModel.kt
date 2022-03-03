c158519b27b0b74576e8c5b3867226e14f4b5055
Shoichi Fukusaka <shoichi.fukusaka@gmail.com>
2022-03-04 02:18:52 +0900

    add fragment/viewmodel

--- app/src/main/java/com/example/android/codesampleapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/android/codesampleapp/ui/main/MainViewModel.kt
@@ -1,8 +1,21 @@
 package com.example.android.codesampleapp.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()
+class MainViewModel @Inject constructor() : ViewModel() {
+    fun onClickButton() {
+        Log.d("VM", "onClickButton")
+    }
+}
